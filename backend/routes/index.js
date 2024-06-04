eaf1d88e53206db9301d458f7eefb12ce409d5dc
Gek <gakidogeck@gmail.com>
2024-06-04 15:12:38 -0700

    added API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+
+router.post('/test', function(req, res) {
+    res.json({ requestBody: req.body });
+});
 
 router.get('/hello/world', function(req, res) {
     res.cookie('XSRF-TOKEN', req.csrfToken());
